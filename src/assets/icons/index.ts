bc19306154bfe0c9e666a3d5346b72c0ae585f06
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-11-25 22:41:28 +0530

    feat: Added top bar for Youtube clone

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,14 @@
+import CreateIcon from "./CreateIcon";
+import MenuIcon from "./MenuIcon";
+import MicIcon from "./MicIcon";
+import NotificationIcon from "./NotificationIcon";
+import SearchIcon from "./SearchIcon";
+import YouTubeIcon from "./YouTubeIcon";
+export {
+    CreateIcon,
+    MenuIcon,
+    MicIcon,
+    NotificationIcon,
+    SearchIcon,
+    YouTubeIcon,
+};
