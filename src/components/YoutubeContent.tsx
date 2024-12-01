8d67c9bd859e4cbc04cb3de00df296b14eb81aed
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-12-01 21:55:16 +0530

    feat: Created Homepage Video Card Component

--- src/components/YoutubeContent.tsx
+++ src/components/YoutubeContent.tsx
@@ -1,4 +1,14 @@
 import "../assets/styles/YoutubeContent.css";
+import VideoCard from "./VideoCard";
+
 export default function YoutubeContent() {
-    return <section id="youtube-content">YoutubeContent</section>;
+    return (
+        <section id="youtube-content">
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+        </section>
+    );
 }
