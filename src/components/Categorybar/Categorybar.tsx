9176236dac601569e4022534923e24a5e05bb22d
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-12-17 10:45:48 +0530

    feat: Added background color to selected category in category bar

--- src/components/Categorybar/Categorybar.tsx
+++ src/components/Categorybar/Categorybar.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 import categories from "../../constants/categories";
 export default function Categorybar() {
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const handleCategoryChange = (category: string) => {
+        setSelectedCategory(category);
+    };
+
+    const getCategoryClass = (category: string) => {
+        return `categories-icon${
+            category === selectedCategory ? " active" : ""
+        }`;
+    };
+
     return (
         <div id="category-bar-container">
             {categories.map((category) => (
-                <p className="categories-icon">{category}</p>
+                <p
+                    className={getCategoryClass(category)}
+                    onClick={() => {
+                        handleCategoryChange(category);
+                    }}
+                >
+                    {category}
+                </p>
             ))}
         </div>
     );
