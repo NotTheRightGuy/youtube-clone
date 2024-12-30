8b1344e724f519ebf122b8b01a30d64108baa995
Janmejay Chatterjee <janmejaychatterjee@gmail.com>
2024-12-30 15:31:24 +0530

    fix: remove typo from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 export default function App() {
     return (
         <main>
-            yar
             <SidebarProvider>
                 <Navbar />
                 <BrowserRouter>
